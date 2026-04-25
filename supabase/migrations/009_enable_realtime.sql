29b6a7815e1e2855217beb590a2f93e7cf9b9230
Le Ngoc Duc <ducdz1m2@gmail.com>
2026-04-25 22:08:27 +0700

    Fix realtime migration to follow Supabase documentation

--- supabase/migrations/009_enable_realtime.sql
+++ supabase/migrations/009_enable_realtime.sql
@@ -1,2 +1,9 @@
 -- Enable Realtime for debts table
+begin;
+-- remove the supabase_realtime publication
+drop publication if exists supabase_realtime;
+-- re-create the supabase_realtime publication with no tables
+create publication supabase_realtime;
+commit;
+-- add debts table to the publication
 alter publication supabase_realtime add table debts;
